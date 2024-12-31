cb221ce89bcafa11758482854ed52650f0c84ad1
PPark8093 <parkjuwon8093@outlook.kr>
2025-01-01 02:03:40 +0900

    alpha

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "../styles/HomePage.css"
+
+function HomePage() {
+  return (
+    <div className='m_container'>
+        <h1 className='m_title'>Minimal Game</h1>
+        <button className='m_button'>틱택토</button>
+    </div>
+  )
+}
+
+export default HomePage
