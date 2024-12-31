36aec9bf846d549f15d7148f10f0737401f89333
PPark8093 <parkjuwon8093@outlook.kr>
2025-01-01 02:34:50 +0900

    alpha 3

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,7 +5,17 @@ function HomePage() {
   return (
     <div className='m_container'>
         <h1 className='m_title'>Minimal Game</h1>
-        <button className='m_button'>틱택토</button>
+        <div className='m_inner_container_grid'>
+          <button className='m_button'>틱택토</button>
+          <button className='m_button'>스네이크</button>
+          <button className='m_button'>반응속도</button>
+          <button className='m_button'>테트리스</button>
+          <button className='m_button'>수학</button>
+          <button className='m_button'>기타</button>
+        </div>
+        <div className='m_inner_container_list'>
+          <button className='m_button'>아이디어 제공</button>
+        </div>
     </div>
   )
 }
