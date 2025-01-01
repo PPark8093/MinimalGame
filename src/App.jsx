1230e3098e62922fa5c094f9ab6fc52c8224cb33
PPark8093 <parkjuwon8093@outlook.kr>
2025-01-01 17:05:09 +0900

    setup home

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import "./styles/default/m_button.css"
-import "./styles/default/m_class.css"
 
 function App() {
     return (
@@ -10,9 +9,6 @@ function App() {
         <Routes>
           <Route path='/' element={<HomePage/>}/>
         </Routes>
-        <footer className="footer">
-                <p>&copy; 2024 <a href='https://github.com/PPark8093' target='_blank'>PPark8093</a>. All rights reserved.</p>
-        </footer>
       </div>
     )
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,25 +1,34 @@
-import React from 'react'
-import "../styles/HomePage.css"
+import React from "react";
+import "../styles/HomePage.css";
 
 function HomePage() {
   return (
-    <div className='m_container'>
-        <header>
-          <h1 className='m_title'>Minimal Game</h1>
-        </header>
-        <div className='m_inner_container_grid'>
-          <button className='m_button'>틱택토</button>
-          <button className='m_button'>스네이크</button>
-          <button className='m_button'>반응속도</button>
-          <button className='m_button'>테트리스</button>
-          <button className='m_button'>수학</button>
-          <button className='m_button'>기타</button>
-        </div>
-        <div className='m_inner_container_list'>
-          <button className='m_button'>아이디어 제공</button>
-        </div>
+    <div className="m_container">
+      <header className="title">
+        <h1 className="m_title">Minimal Game</h1>
+      </header>
+      <div className="m_inner_container_grid">
+        <button className="m_button">틱택토</button>
+        <button className="m_button">스네이크</button>
+        <button className="m_button">반응속도</button>
+        <button className="m_button">테트리스</button>
+        <button className="m_button">수학</button>
+        <button className="m_button">기타</button>
+      </div>
+      <div className="m_inner_container_list">
+        <button className="m_button">아이디어 제공하기</button>
+      </div>
+      <footer className="copyright">
+        <p>
+          &copy; 2024{" "}
+          <a href="https://github.com/PPark8093" target="_blank">
+            PPark8093
+          </a>
+          . All rights reserved.
+        </p>
+      </footer>
     </div>
-  )
+  );
 }
 
-export default HomePage
+export default HomePage;
